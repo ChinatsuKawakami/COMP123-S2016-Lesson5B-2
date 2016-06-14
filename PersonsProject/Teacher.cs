4d062d51f0070e5d527707eedf442bacecc31ba0
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-06-13 23:53:42 -0400

    added Person project and student, teacher,domestic student, international student class

---
+++ PersonsProject/Teacher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PersonProject
+{
+    public class Teacher : Person
+    {
+        public int EmployeeNumber
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public void Teach()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
